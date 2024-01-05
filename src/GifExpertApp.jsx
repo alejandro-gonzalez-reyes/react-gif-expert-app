1acdb16797ef97a908db4021afa794c5d9a3c97d
Alejandro González Reyes <lic.alejandro.gonzalez.reyes@gmail.com>
2024-01-05 12:31:54 -0600

    componente GifExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,12 @@
 export const GifExpertApp = () => {
   return (
     <>
+      {/* Titulo de la aplicación */}
       <h1>Gif Expert App</h1>
+
+      {/* Buscador de Gifs */}
+
+      {/* Listado de Gifs relacionados con la búsqueda */}
     </>
   );
 };
