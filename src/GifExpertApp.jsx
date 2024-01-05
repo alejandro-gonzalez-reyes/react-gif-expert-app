e391d1e86f9a8bbd150446b9f8ef898fdc852905
Alejandro González Reyes <lic.alejandro.gonzalez.reyes@gmail.com>
2024-01-05 17:54:50 -0600

    crear listado de categorías

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+  // Siempre que se desee cambiar parte del contenido del HTML de forma dinámica, se debe pensar en el estado interno del componente
+  // Los hooks en React jamás deben condicionarse
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Dragon Ball",
+    "Pokemon",
+    "Super Campeones",
+  ]);
   return (
     <>
       {/* Titulo de la aplicación */}
@@ -7,6 +17,15 @@ export const GifExpertApp = () => {
       {/* Buscador de Gifs */}
 
       {/* Listado de Gifs relacionados con la búsqueda */}
+      <ol>
+        {/* 
+        Iterar el contenido de nuestro estado de categorías e imprimirlas en pantalla.
+        Es importante que para cada elemento, se le asigne un key, el cuál sirve como variable de control para que React tenga conocimiento que elemento entra y se destruye en el componente
+        */}
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
   );
 };
